73f8e67993d882ae54972dba59c15cbae9039ba6
zirtoshka <367222@edu.itmo.ru>
2024-04-15 23:32:57 +0300

    points+style

--- src/app/comp/points/points.component.ts
+++ src/app/comp/points/points.component.ts
@@ -3,24 +3,61 @@ import {AppService} from "../../app.service";
 import {error} from "@angular/compiler-cli/src/transformers/util";
 import {GraphComponent} from "../graph/graph.component";
 import {Respon} from "../../response";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+interface Point {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-points',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './points.component.html',
   styleUrl: './points.component.css'
 })
+
+
 export class PointsComponent {
   private appService = inject(AppService);
 
+
   @Output() choseEvent = new EventEmitter<Respon>();
+
+
+  pointsForm: FormGroup[] = [];
+  readonly maxPoints = 12;
+
+  constructor(private fb: FormBuilder) {
+    this.addPoint(); // Добавляем первую точку при инициализации
+  }
+
+  addPoint() {
+    if (this.pointsForm.length < this.maxPoints) {
+      const pointFormGroup = this.fb.group({
+        "x": ["0", [Validators.required, Validators.pattern('-?\\d+([\\.,]\\d+)?')]],
+        "y": ['0', [Validators.required, Validators.pattern('-?\\d+([\\.,]\\d+)?')]]
+      });
+      this.pointsForm.push(pointFormGroup);
+      console.info(this.pointsForm)
+    }
+  }
+
+  removePoint(index: number) {
+    this.pointsForm.splice(index, 1);
+  }
 
   pointsSend() {
     this.appService.approxRequest().subscribe({
       next: (response) => {
         // console.info(response.linear);
-      //   here i need
+        //   here i need
         this.choseEvent.emit(response);
 
       },
@@ -29,4 +66,5 @@ export class PointsComponent {
       }
     })
   }
+
 }
